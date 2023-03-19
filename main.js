f969ce7b3dbadccd4ca51342849d91acc73e71ec
Alon Fine <mecheniy5462@gmail.com>
2023-03-20 00:33:57 +0200

    add ability to resive number from input and use this num as count of cards

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
 import Card from "./card.js"
 
-function createGame(container, countCards) {
+function createGame(container) {
     // create game field
     let mainArr = [],
         cardsArr = [],
         firstCard = null,
         secondCard = null,
-        timer = 5,
+        timer = 15,
         timeToEnd,
-        newGame = true
+        countCards
 
     container.innerHTML = ''
-
-    for (let digit = 1; digit <= countCards / 2; digit++) {
-        mainArr.push(digit, digit);
-    }
-    mainArr = mainArr.sort(() => Math.random() - 0.5)
-
-    for (let el = 0; el < mainArr.length; el++) {
-        cardsArr.push(new Card(container, mainArr[el], activity));
-    }
 
     function field(container, timer) {
         let $input,
@@ -28,7 +19,7 @@ function createGame(container, countCards) {
             $title,
             $text
 
-        $wrapper = document.createElement('div')
+        $wrapper = document.createElement('form')
         $wrapper.classList.add('wrapper')
         $input = document.createElement('input')
         $input.classList.add('input')
@@ -48,87 +39,107 @@ function createGame(container, countCards) {
         container.append($wrapper)
 
         return {
+            $wrapper,
             $display,
             $input,
             $text,
             $title
         }
     }
-
-    let createField = field(container, timer)
-
     function endGame(container, countCards) {
         createField.$display.innerHTML = 'time out'
+        timer = 15
         setTimeout(() => {
             mainArr = []
             cardsArr = []
             firstCard = null
             secondCard = null
-            timer = 5
-            newGame = true
             clearInterval(timeToEnd)
-        }, 900)
-        createGame(container, countCards)
+        }, 1000)
+        createGame(container)
+    }
+    // CONDITION FOR LOSE
+    function loseConditions() {
+        clearInterval(timeToEnd)
+        timeToEnd = setInterval(() => {
+            if (timer > 0) {
+                timer--
+                createField.$display.innerHTML = timer
+            } else {
+                createField.$display.innerHTML = 'time out'
+                setTimeout(() => {
+                    alert('YOU LOSE!');
+                    endGame(container, countCards);
+                }, 900);
+                clearInterval(timeToEnd);
+            }
+        }, 1000)
     }
 
-    function activity(obj) {
-        // logic
-        if (firstCard !== null && secondCard !== null) {
-            if (firstCard.number != secondCard.number) {
-                firstCard.open = false
-                secondCard.open = false
-                firstCard = null
-                secondCard = null
-            }
+    let createField = field(container, timer)
+
+    // get value of input and assign it like count of cards
+    createField.$wrapper.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if (!createField.$input.value) {
+            return
         }
-        if (firstCard == null) {
-            firstCard = obj
-            newGame = false
-        } else if (secondCard == null) {
-            secondCard = obj
+        countCards = createField.$input.value
+        loseConditions()
+
+        for (let digit = 1; digit <= countCards / 2; digit++) {
+            mainArr.push(digit, digit);
+        }
+        mainArr = mainArr.sort(() => Math.random() - 0.5)
+
+        for (let el = 0; el < countCards; el++) {
+            cardsArr.push(new Card(container, mainArr[el], activity));
         }
 
-        if (firstCard != null && secondCard != null) {
-            if (firstCard.number == secondCard.number) {
-                firstCard.match = true
-                secondCard.match = true
-                firstCard = null
-                secondCard = null
-            } else {
-                // hide card if is'nt match 
-                setTimeout(() => {
+        function activity(obj) {
+            // logic
+            if (firstCard !== null && secondCard !== null) {
+                if (firstCard.number != secondCard.number) {
                     firstCard.open = false
                     secondCard.open = false
                     firstCard = null
                     secondCard = null
-                }, 500)
+                }
+            }
+            if (firstCard == null) {
+                firstCard = obj
+            } else if (secondCard == null) {
+                secondCard = obj
+            }
+
+            if (firstCard != null && secondCard != null) {
+                if (firstCard.number == secondCard.number) {
+                    firstCard.match = true
+                    secondCard.match = true
+                    firstCard = null
+                    secondCard = null
+                } else {
+                    // hide card if is'nt match 
+                    setTimeout(() => {
+                        firstCard.open = false
+                        secondCard.open = false
+                        firstCard = null
+                        secondCard = null
+                    }, 500)
+                }
+            }
+
+            // CONDISHION FOR WINN
+            if (container.querySelectorAll('.match').length == cardsArr.length && cardsArr.length !== 0) {
+                setTimeout(() => {
+                    alert('YOU WINN!')
+                    endGame(container, countCards);
+                }, 1000);
             }
         }
-
-        // CONDISHION FOR WINN
-        if (container.querySelectorAll('.match').length == cardsArr.length && cardsArr.length !== 0) {
-            setTimeout(() => {
-                alert('YOU WINN!')
-                endGame(container, countCards);
-            }, 1000);
-        }
-        // CONDITION FOR LOSE
-        if (obj.open == true) {
-            clearInterval(timeToEnd)
-            timeToEnd = setInterval(() => {
-                if (timer > 0) {
-                    timer--
-                    createField.$display.innerHTML = timer
-                } else {
-                    createField.$display.innerHTML = 'time out'
-                    setTimeout(() => {
-                        alert('YOU LOSE!');
-                        endGame(container, countCards);
-                    }, 1000);
-                }
-            }, 1000)
-        }
-    }
+        createField.$input.value = ''
+        createField.$input.disabled = true
+    })
 }
 
-createGame(document.getElementById('game'), 6)
+createGame(document.getElementById('game'))
